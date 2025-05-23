7e0f5bbd797b21eb0c742c26a6de721ba5862b65
NguyenHuynhNhatDuy2004 <nhatduy3q@gmail.com>
2025-05-23 21:13:54 +0700

    sua

--- WebQuanLyKhohang/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebQuanLyKhohang.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
